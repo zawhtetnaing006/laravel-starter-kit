cda6fbd097c2b2314bfef716e3341e58e8ddcdbd
Zaw Htet Naing <yourbiddaddy@gmail.com>
2023-06-11 16:46:00 +0700

    add application initiation command

---
+++ app/Console/Commands/AppInit.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class AppInit extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:init';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Artisan::call('key:generate');
+        Artisan::call('migrate');
+        Artisan::call('db:seed');
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
+])->prefix('admin')->group(function () {
+    Route::get('/', function () {
         return view('dashboard');
     })->name('dashboard');
 });
